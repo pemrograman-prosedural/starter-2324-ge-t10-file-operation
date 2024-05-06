1cf789d9a707ed2714fe3d48be9de13ea9a92fce
chandropardede <45875657+chandropardede@users.noreply.github.com>
2024-05-06 14:00:44 +0700

    Initial commit

---
+++ t10_02.c
@@ -0,0 +1,13 @@
+// NIM - Name
+// NIM - Name
+
+#include <stdio.h>
+#include "./libs/dorm.h"
+#include "./libs/student.h"
+#include "./libs/repository.h"
+
+int main(int _argc, char **_argv)
+{
+    // codes
+    return 0;
+}
